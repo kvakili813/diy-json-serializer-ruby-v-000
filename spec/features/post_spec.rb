54b625740b42b7577e1c3a216a970ca31821284d
Kayvan Vakili <kvakili813@gmail.com>
2018-09-11 03:25:45 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+require 'pry'
 describe 'navigate' do
   before do
     @post = Post.create(title: "My Post", description: "My post desc")
@@ -7,6 +7,7 @@ describe 'navigate' do
 
   it 'shows the title on the show page in a h1 tag' do
     visit post_path(@post)
+    binding.pry
     expect(page).to have_css("h1", text: "My Post")
   end
 
